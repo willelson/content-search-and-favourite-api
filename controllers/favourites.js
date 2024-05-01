5e652e4fbc8c8a0cd2bce0a1f263d8e652e2553b
Will Elson <elson594@gmail.com>
2024-05-01 04:47:36 +0100

    Update controller definitions

--- controllers/favourites.js
+++ controllers/favourites.js
@@ -52,100 +52,95 @@ exports.getFavourites =
     res.status(200).json(response);
   });
 
-exports.addFavourite =
-  ('',
-  async (req, res, next) => {
-    // request body should include pixabay content ID
-    const { pixabayId } = req.body;
+exports.addFavourite = async (req, res, next) => {
+  // request body should include pixabay content ID
+  const { pixabayId } = req.body;
 
-    // Check required parameters are present
-    if (!pixabayId || !req.body.contentType) {
-      res.status(400).json({
-        errors: ['pixabayId and contentType parameters are required']
-      });
-      return;
-    }
-
-    // Check content type is valid
-    if (!contentTypeValid(req.body.contentType)) {
-      res
-        .status(400)
-        .json({ errors: ['contentType must be either video or image'] });
-      return;
-    }
-
-    // Check pixabayId is a valid number
-    if (!pixabayIdValid(pixabayId)) {
-      res.status(400).json({ errors: ['pixabayId must be a valid integer'] });
-      return;
-    }
-
-    // Check if a favourite with this ID already exists in the table
-    let favourite = await Favourite.findOne({
-      where: { pixabay_id: pixabayId, content_type: req.body.contentType }
+  // Check required parameters are present
+  if (!pixabayId || !req.body.contentType) {
+    res.status(400).json({
+      errors: ['pixabayId and contentType parameters are required']
     });
+    return;
+  }
 
-    // If not, fetch content data from Pixabay api
-    let favouriteData = {};
-    if (!favourite) {
-      try {
-        favouriteData = await fetchContentById(pixabayId, req.body.contentType);
-      } catch (err) {
-        return next(err);
-      }
+  // Check content type is valid
+  if (!contentTypeValid(req.body.contentType)) {
+    res
+      .status(400)
+      .json({ errors: ['contentType must be either video or image'] });
+    return;
+  }
 
-      // Return a 404 if no result comes back from Pixabay
-      if (!favouriteData) {
-        console.log('no fav data');
-        res.status(404).json({ errors: ['Content not found'] });
-        return;
-      }
+  // Check pixabayId is a valid number
+  if (!pixabayIdValid(pixabayId)) {
+    res.status(400).json({ errors: ['pixabayId must be a valid integer'] });
+    return;
+  }
 
-      const { contentType, thumbnail, contentURL, pixabayURL } = favouriteData;
-
-      favourite = await Favourite.create({
-        pixabay_id: pixabayId,
-        content_type: contentType,
-        thumbnail_url: thumbnail,
-        content_url: contentURL,
-        pixabay_url: pixabayURL
-      });
-    }
-
-    // Add favourite to the users favourites
-    await req.user.addFavourite(favourite);
-
-    // TODO return newly created object as json
-    const msg = `Favourite content (id: ${favourite.pixabay_id}, type: ${favourite.content_type}) successfully added for user ${req.user.email}`;
-    res.status(201).json({ message: msg });
+  // Check if a favourite with this ID already exists in the table
+  let favourite = await Favourite.findOne({
+    where: { pixabay_id: pixabayId, content_type: req.body.contentType }
   });
 
-exports.deleteFavourite =
-  ('',
-  async (req, res, next) => {
-    const { favouriteId } = req.params;
+  // If not, fetch content data from Pixabay api
+  let favouriteData = {};
+  if (!favourite) {
+    try {
+      favouriteData = await fetchContentById(pixabayId, req.body.contentType);
+    } catch (err) {
+      return next(err);
+    }
 
-    // TODO Check favouriteId is pass and is a valid number
-
-    // Find record in database with favouriteId
-    const favourite = await Favourite.findOne({
-      where: { id: parseInt(favouriteId) }
-    });
-
-    // Check the favourite exists
-    if (!favourite) {
+    // Return a 404 if no result comes back from Pixabay
+    if (!favouriteData) {
       res.status(404).json({ errors: ['Content not found'] });
       return;
     }
 
-    // Check user has this favourite
-    const userHasFavourite = await req.user.hasFavourite(favourite);
-    if (!favourite) {
-      res.status(404).json({ errors: ['Content not found'] });
-      return;
-    }
+    const { contentType, thumbnail, contentURL, pixabayURL } = favouriteData;
 
-    req.user.removeFavourite(favourite);
+    favourite = await Favourite.create({
+      pixabay_id: pixabayId,
+      content_type: contentType,
+      thumbnail_url: thumbnail,
+      content_url: contentURL,
+      pixabay_url: pixabayURL
+    });
+  }
 
-    res.status(202).send();
+  // Add favourite to the users favourites
+  await req.user.addFavourite(favourite);
+
+  // TODO return newly created object as json
+  const msg = `Favourite content (id: ${favourite.pixabay_id}, type: ${favourite.content_type}) successfully added for user ${req.user.email}`;
+  res.status(201).json({ message: msg });
+};
+
+exports.deleteFavourite = async (req, res, next) => {
+  const { favouriteId } = req.params;
+
+  // TODO Check favouriteId is pass and is a valid number
+
+  // Find record in database with favouriteId
+  const favourite = await Favourite.findOne({
+    where: { id: parseInt(favouriteId) }
   });
+
+  // Check the favourite exists
+  if (!favourite) {
+    res.status(404).json({ errors: ['Content not found'] });
+    return;
+  }
+
+  // Check user has this favourite
+  const userHasFavourite = await req.user.hasFavourite(favourite);
+  if (!favourite) {
+    res.status(404).json({ errors: ['Content not found'] });
+    return;
+  }
+
+  req.user.removeFavourite(favourite);
+
+  res.status(202).send();
+};
--- controllers/search.js
+++ controllers/search.js
@@ -12,37 +12,35 @@ const {
  * - contentType @required - type of content to fetch from Pixabay must only be 'image' or 'video'
  * - page - page number
  */
-exports.search =
-  ('',
-  async (req, res, next) => {
-    const { query, contentType, page } = req.query;
+exports.search = async (req, res, next) => {
+  const { query, contentType, page } = req.query;
 
-    // Check required parameters are present
-    if (!query || !contentType) {
-      res
-        .status(400)
-        .json({ errors: ['query and contentType parameters are required'] });
-      return;
-    }
+  // Check required parameters are present
+  if (!query || !contentType) {
+    res
+      .status(400)
+      .json({ errors: ['query and contentType parameters are required'] });
+    return;
+  }
 
-    // Check content type is valid
-    if (!contentTypeValid(contentType)) {
-      res
-        .status(400)
-        .json({ errors: ['contentType must be either video or image'] });
-      return;
-    }
+  // Check content type is valid
+  if (!contentTypeValid(contentType)) {
+    res
+      .status(400)
+      .json({ errors: ['contentType must be either video or image'] });
+    return;
+  }
 
-    // Check page number is valid
-    if (!pageNumberValid(page)) {
-      res.status(400).json({ errors: ['page must be a valid number'] });
-      return;
-    }
+  // Check page number is valid
+  if (!pageNumberValid(page)) {
+    res.status(400).json({ errors: ['page must be a valid number'] });
+    return;
+  }
 
-    try {
-      const data = await fetchContent(query, contentType, page, next);
-      res.status(200).json(data);
-    } catch (err) {
-      return next(err);
-    }
-  });
+  try {
+    const data = await fetchContent(query, contentType, page, next);
+    res.status(200).json(data);
+  } catch (err) {
+    return next(err);
+  }
+};
